073a32dc02ed1bc30c3677e138fd22e8142acbd3
Robin Beltran <robinbeltran@MacBook-Pro-de-Robin.local>
2020-01-02 21:09:52 -0600

    validacion de titulo de la lista de citas

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -3,18 +3,22 @@ import Cita from "./Cita";
 
 import PropTypes from "prop-types";
 
-const ListaCitas = ({ citas, eliminarCita }) => (
-  <div className="card mt-2 py-5">
-    <div className="card-body">
-      <h2 className="card-title text-center">Administra las citas</h2>
-      <div className="lista-citas">
-        {citas.map(citas => (
-          <Cita key={citas.id} cita={citas} eliminarCita={eliminarCita} />
-        ))}
+const ListaCitas = ({ citas, eliminarCita }) => {
+  const mensaje =
+    Object.keys(citas).length === 0 ? "No hay citas" : "Administra las citas";
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title text-center">{mensaje}</h2>
+        <div className="lista-citas">
+          {citas.map(citas => (
+            <Cita key={citas.id} cita={citas} eliminarCita={eliminarCita} />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 ListaCitas.prototype = {
   citas: PropTypes.array.isRequired,
